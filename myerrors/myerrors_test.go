8ef70daa20085c79c124678731c0d45239ca440d
akaKAIN <mkain@mail.ru>
2021-02-23 16:37:51 +1000

    Added tests for methods of errors creation

--- myerrors/myerrors_test.go
+++ myerrors/myerrors_test.go
@@ -4,6 +4,28 @@ import (
 	"testing"
 )
 
-func NewErrorTimeTest(t *testing.T) {}
+func NewErrorTimeTest(t *testing.T) {
+	message := "error error"
+	err := NewErrorTime(message)
 
-func NewErrorTraceTest(t *testing.T) {}
+	errStruct, ok := err.(*ErrorWithTime)
+	if !ok {
+		t.Fatal("Wrong error struct")
+	}
+	if errStruct.Text != message {
+		t.Fatalf("wrong message text. Want: %s, Get: %s\n", message, errStruct.Text)
+	}
+}
+
+func NewErrorTraceTest(t *testing.T) {
+	message := "error error"
+	err := NewErrorTime(message)
+
+	errStruct, ok := err.(*ErrorWithStackTrace)
+	if !ok {
+		t.Fatal("Wrong error struct")
+	}
+	if errStruct.Text != message {
+		t.Fatalf("wrong message text. Want: %s, Get: %s\n", message, errStruct.Text)
+	}
+}
