fafead9c13bd5dda91845b08a351bb4feef59248
akaKAIN <mkain@mail.ru>
2021-03-14 13:49:28 +1000

    Init course work

---
+++ finder/finder.go
@@ -0,0 +1,98 @@
+package finder
+
+import (
+	"errors"
+	"fmt"
+	"os"
+	"path/filepath"
+	"time"
+)
+
+var (
+	ErrorFileDoesNotExist = errors.New("file does not exist")
+	ErrorFileIsDir        = errors.New("the passed file is a dir")
+	ErrorPathDoesNotExist = errors.New("path does not exist")
+	ErrorPathIsNotDir     = errors.New("the passed path is not a dir")
+)
+
+// Фукция для поиска совпадений файлов по имени и размеру.
+// Получает на вход адрес директории для поиска и имя файла для поиска
+func FindCopy(path, fileName string) ([]string, error) {
+	if err := ValidateDirPath(path); err != nil {
+		return nil, err
+	}
+	if err := ValidateFilePath(fileName); err != nil {
+		return nil, err
+	}
+
+	var (
+		copyFilePathCh = make(chan string)
+		copyList       = make([]string, 0)
+		checker        Checker
+	)
+	checker, err := NewSearchTarget(fileName)
+	if err != nil {
+		return nil, fmt.Errorf("error of init search target struct: %w", err)
+	}
+	go func() {
+		for {
+			select {
+			case copyPath, ok := <-copyFilePathCh:
+				if ok {
+					copyList = append(copyList, copyPath)
+				} else {
+					break
+				}
+			case <-time.After(10 * time.Second):
+				fmt.Println("Timeout. Channel reader is dead")
+				return
+			}
+		}
+	}()
+
+	checker.WgAdd()
+	WalkInDir(path, checker, copyFilePathCh)
+	checker.WgDone()
+
+	checker.WgWait()
+	close(copyFilePathCh)
+	fmt.Println(copyList)
+	return copyList, nil
+}
+
+// Функция выполняет чтение содержимого директории.
+// Найденные директории рекурсивно вызывают WalkInDir.
+// Файлы проверяются на копирование. Если факт соответствия копии исходнику подтверждается, то
+// путь к файлу отправляется в канал fileCh.
+func WalkInDir(path string, c Checker, fileCh chan<- string) {
+	c.WgAdd()
+	defer c.WgDone()
+	if err := os.Chdir(path); err != nil {
+		return
+	}
+	dirList, err := os.ReadDir(path)
+	if err != nil {
+		return
+	}
+
+	for _, dirFile := range dirList {
+		filePath, err := filepath.Abs(filepath.Join(path, dirFile.Name()))
+		if err != nil {
+			continue
+		}
+
+		fi, err := dirFile.Info()
+		if err != nil {
+			continue
+		}
+
+		if c.Check(fi, filePath) {
+			fileCh <- filePath
+		}
+
+		if dirFile.IsDir() {
+			go WalkInDir(filePath, c, fileCh)
+			continue
+		}
+	}
+}
--- main/hw.go
+++ main/hw.go
@@ -1,36 +1,54 @@
 package main
 
 import (
+	"bufio"
+	"flag"
 	"fmt"
-	"github.com/akaKAIN/go-gb-level-2/workers"
-	"math/rand"
-	"time"
+	"github.com/akaKAIN/go-gb-level-2/finder"
+	"log"
+	"os"
+	"strings"
+)
+
+var (
+	path *string
+	file *string
+	d    *bool
 )
 
 func init() {
-	rand.Seed(time.Now().UnixNano())
+	path = flag.String("path", ".", "dir for search")
+	file = flag.String("file", "", "file name for search")
+	d = flag.Bool("d", false, "Need to delete found copy files?")
+	flag.Parse()
 }
 
-// 1. С помощью пула воркеров написать программу, которая запускает 1000 горутин,
-// каждая из которых увеличивает число на 1. Дождаться завершения всех горутин
-// и убедиться, что при каждом запуске программы итоговое число равно 1000.
-
-// 2. Написать программу, которая при получении в канал сигнала SIGTERM
-// останавливается не позднее, чем за одну секунду (установить таймаут).
 func main() {
-	var counter int
-	jobHandler := func() {counter++}
-	workers.WorkerHandler(1000, jobHandler)
-	fmt.Printf("Counter: %d\n", counter)
+	copyList, err := finder.FindCopy(*path, *file)
+	if err != nil {
+		log.Fatalf("find error: %v", err)
+	}
+	copyCount := len(copyList)
 
-	//workers.Start()
-	//mysign.SoftShotDown([]os.Signal{syscall.SIGINT}, os.Stdout, "well done")
-
-	channelBuffer := 5
-	ch := make(chan int, channelBuffer)
-	for i:=1; i<=channelBuffer; i++ {
-		ch <- i
+	if copyCount > 0 {
+		fmt.Printf("Found copies: %d\n", copyCount)
+		for _, copyName := range copyList {
+			fmt.Println(copyName)
+		}
+	} else {
+		fmt.Printf("No copy of %q in path: %q\n", *file, *path)
+		return
 	}
 
-	workers.SleepingPool(3, ch, time.Second)
+	scanner := bufio.NewScanner(os.Stdin)
+	if *d {
+		fmt.Print("\nAre you sure you want to delete the copies? (for confirm enter: 'yes'):\t")
+		scanner.Scan()
+		userAnswer := scanner.Text()
+		if strings.ToLower(userAnswer) == "yes" {
+			for _, copyName := range copyList {
+				fmt.Println("- deleted:", copyName)
+			}
+		}
+	}
 }
